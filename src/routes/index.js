9bd14d37230a4d7b18619c2c273654209117a7d7
Chris Barber-Riley <chris.barberriley@gmail.com>
2020-12-09 20:22:24 +0000

    Setup routing

---
+++ src/routes/index.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const axios = require('axios');
+const router = express.Router();
+
+let cachedData;
+let cachedTime;
+
+// @desc        Register user
+// @route       GET /fetch-data
+// @access      Private
+router.get('/fetch-data', async (req, res) => {
+  // Check if user is reloading within 30 seconds and serve up memory cache
+  if (cachedTime && cachedTime > Date.now() - 30 * 1000) {
+    return cachedData;
+  }
+
+  try {
+    // Declare base url & params
+    const BASE_URL = `https://api.airtable.com/v0/app3moXo7so1Q7p7s/Bugs%20and%20issues?`;
+    const PARAMS = new URLSearchParams({ maxRecords: 3 });
+    const config = {
+      headers: { Authorization: `Bearer ${process.env.BEARER}` },
+    };
+
+    // Make request for data from Air Table
+    const { data } = await axios.get(`${BASE_URL}${PARAMS}`, config);
+
+    cachedData = data;
+    cachedTime = Date.now();
+    data.cachedTime = cachedTime;
+
+    return res.json(data);
+  } catch (error) {
+    console.error(error);
+    return res.json(error);
+  }
+});
+
+module.exports = router;
--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 const path = require('path');
-const axios = require('axios');
+const routes = require('./routes/index.js');
 
 const app = express();
 
@@ -16,22 +16,7 @@ if (process.env.NODE_ENV == 'development') {
 app.use(cors());
 app.use(express.json());
 
-app.get('/fetch-data', async (req, res) => {
-  try {
-    const BASE_URL = `https://api.airtable.com/v0/app3moXo7so1Q7p7s/Bugs%20and%20issues?`;
-    const PARAMS = new URLSearchParams({ maxRecords: 3 });
-    const config = {
-      headers: { Authorization: `Bearer ${process.env.BEARER}` },
-    };
-    console.log(BASE_URL);
-    const { data } = await axios.get(`${BASE_URL}${PARAMS}`, config);
-    return res.json(data);
-  } catch (error) {
-    console.error(error);
-    return res.json(error);
-  }
-});
-
+app.use('/', routes);
 app.use(express.static(__dirname + '/public'));
 
 app.get('*', (req, res) => {
